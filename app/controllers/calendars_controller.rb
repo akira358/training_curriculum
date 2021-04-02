f127cc94f9233376a9cd5acc19192e647577d623
akira358 <yamazakiakira@yamazakiakiranoMacBook-Air.local>
2021-04-02 15:42:30 +0900

    Issue6の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -19,14 +19,9 @@ class CalendarsController < ApplicationController
   end
 
   def get_week
-<<<<<<< Updated upstream
     
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
     
-=======
-    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-
->>>>>>> Stashed changes
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
@@ -41,24 +36,14 @@ class CalendarsController < ApplicationController
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
-<<<<<<< Updated upstream
       end
       
       wday_num = Date.today.wday + x  # wdayメソッドを用いて取得した数値
-      if #「wday_numが7以上の場合」という条件式
+      if wday_num >= 7#「wday_numが7以上の場合」という条件式
         wday_num = wday_num -7
       end
 
        days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :wday => wdays[wday_num], :plans => today_plans} 
-=======
-      end
-        wday_num = Date.today.wday + x  
-      if wday_num >= 7
-        wday_num = wday_num -7
-      end
-
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :wday => wdays[wday_num], :plans => today_plans} 
->>>>>>> Stashed changes
        @week_days.push(days)
     end
 
